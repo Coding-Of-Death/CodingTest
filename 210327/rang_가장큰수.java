2778b2fcb0c1140845bcf89633ce52b6c0e5f965
DyeonKim <45352173+DyeonKim@users.noreply.github.com>
2021-04-02 04:10:55 +0900

    Update rang_가장큰수.java
    내가 만든 코드의 문제점
    1. 가장 큰자리 수의 배열을 만들어 정렬했으나 order 배열과 연결하지 못함.
    2. 가장 큰자리 수와 기존 배열의 위치를 변수로 가지는 클래스를 만들어 해결해보려 했으나 일반적인 정렬로는 객체를 정렬할 수 없어서 실패
    3. 객체를 정렬하기 위해서는 Comparable 인터페이스를 사용하거나 Comparator 객체를 사용
        3-1. 정렬하고자 하는 객체에 이미 존재하고 있는 정렬 기준과 다른 정렬 기준으로 정렬을 하고자 하는 것이므로 Comparator 객체 사용이 알맞음
        3-2. https://www.daleseo.com/java-comparable-comparator/
    4. a.compareTo.(b) 에서 a와 b의 순서 주의
    
    느낀점 : 객체정렬의 중요성, 정렬함수를 이용한 순열,,, 문자열로 바꾸지 않고 정렬하는 코드도 도전해보자

--- 210327/rang_가장큰수.java
+++ 210327/rang_가장큰수.java
@@ -4,37 +4,40 @@ class Solution {
     
     public String solution(int[] numbers) {
         String answer = "";
-        int num = 0;
+        String[] strNums = new String[numbers.length];
         
-        int[] headNum   = new int[numbers.length];
-        int[][] order   = new int[numbers.length][2];
-        
-        for (int i = 0; i < numbers.length; i++) {            
-            if      (numbers[i] / 100  >= 1)  num = numbers[i] / 100;
-            else if (numbers[i] / 10   >= 1)  num = numbers[i] / 10;
-            else    num = numbers[i];
-            
-            headNum[i] = num;
-            order[i][0] = num;
-            order[i][1] = i;
+        for (int i = 0; i < numbers.length; i++) {
+        	strNums[i] = "" + numbers[i];
         }
         
-        Arrays.sort(headNum);
+        /*
+        Comparator 객체를 사용해야할 때
+        - 정렬 대상 클래스의 코드를 직접 수정할 수 없는 경우
+        - 정렬하고자 하는 객체에 이미 존재하고 있는 정렬 기준과 다른 정렬 기준으로 정렬을 하고 싶을 때
         
-        for (int i = headNum.length - 1; i >= 0; i--) {
-            if (i > 0 && order[i][0] == order[i - 1][0]) {
-                String con1 = 
-                    "" + numbers[order[i][1]] + numbers[order[i - 1][1]];
-                String con2 = 
-                    "" + numbers[order[i - 1][1]] + numbers[order[i][1]];
-                
-                num = Math.max(Integer.parsInt(con1), Integer.parsInt(con2));
-            } else {
-                num = numbers[order[i][1]];
-            }
-            
-            answer += num;
+        a.compareTo(b) : 
+            1.  compareTo() 메서드를 통해 인자로 넘어온 같은 타입의 다른 객체와 대소 비교가 가능
+            2. 앞에서부터 비교하다 다른 문자열이 나오면 'a-b' 순서로 해당 문자의 아스키코드 값을 뺀 결과(int)를 리턴
+            3. 메서드를 호출하는 객체가 인자로 넘어온 객체보다 작을 경우에는 음수를 리턴하고, 크기가 동일하다면 0, 클 경우에는 양수를 리턴(4번에서 순서가 중요한 이유!)
+            4. a, b 순서로 있을 때 (b+a).compareTo(a+b) 을 했을 경우 'b+a'가 더 크다면 자리를 바꿔주면 된다.
+        */
+        Arrays.sort(strNums, new Comparator<String>() {
+        	@Override
+        	public int compare(String num1, String num2) {
+        		return (num2 + num1).compareTo(num1 + num2);
+        	}
+        });
+        // 람다식 표현
+        //Arrays.sort(strNums, (num1, num2) -> (num2 + num1).compareTo(num1 + num2));
+        
+        if (strNums[0].equals("0")) {
+        	return "0";
         }
+        
+        for (String str : strNums) {
+        	answer += str;
+        }
+        
         
         return answer;
     }
