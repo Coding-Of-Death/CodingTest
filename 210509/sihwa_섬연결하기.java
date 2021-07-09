4adf09a710201b6cf5743700d7ca575138cd5aac
sihwa1019 <62604614+sihwa1019@users.noreply.github.com>
2021-07-09 21:30:55 +0900

    Create sihwa_섬연결하기.java

---
+++ 210509/sihwa_섬연결하기.java
@@ -0,0 +1,48 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+        Arrays.sort(costs,(o1,o2)->{
+            return o1[2]-o2[2];
+        });
+        int[] parent = new int[n];
+        
+        for(int i = 0; i <n; i++){
+            parent[i]=i;
+        }
+        
+        for(int[] cost : costs){
+            int from = cost[0];
+            int to = cost[1];
+            int value = cost[2];
+            
+            if(connectCheck(parent, from, to)) continue;
+            else{
+                answer += value;
+                union(parent, from, to);
+            }
+        }
+        
+        return answer;
+    }
+    
+    private static void union(int[] parent, int from, int to){
+        from = getParent(parent,from);
+        to = getParent(parent, to);
+        
+        if(from < to)
+            parent[to] = from;
+        else
+            parent[from] = to;
+    }
+    
+    private static boolean connectCheck(int[] parent, int from, int to){
+        from = getParent(parent, from);
+        to = getParent(parent, to);
+        return from == to;
+    }
+    private static int getParent(int[] parent, int edge){
+        if(parent[edge] == edge) return edge;
+        return getParent(parent, parent[edge]);
+    }
+}
