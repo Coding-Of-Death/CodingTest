0907a609fe5d517b1ccdd68dcade3b2e84877a06
sihwa1019 <62604614+sihwa1019@users.noreply.github.com>
2021-07-11 15:19:44 +0900

    Create sihwa_전화번호 목록.java

---
+++ 210328/sihwa_전화번호 목록.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public boolean solution(String[] phone_book) {
+       boolean answer = true;
+       HashMap<String, String> hm = new HashMap<>();
+		
+		
+		for( String input : phone_book ) {
+			hm.put(input, input);
+		}
+		
+		
+		for ( String target : phone_book) {
+			
+			for( int i=0; i< target.length(); i++) {
+				
+				if( hm.containsKey(target.substring(0,i)) ) {
+					return false;
+				}
+				
+				/*else if ( hm.get(target.substring(0,i)) != null) {
+					return false;
+				}*/
+			}
+			
+		}
+        return answer;
+    }
+    
+}
