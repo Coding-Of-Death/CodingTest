c64d8288a012bed85e357eab3c8c223f86b3963c
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-12-14 23:40:04 +0900

    Create LCS.java

---
+++ 210717/과제/LCS.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main{
+    static Integer[][] dp;
+    static char[] str1, str2;
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        str1 = sc.nextLine().toCharArray();
+        str2 = sc.nextLine().toCharArray();
+        
+        dp = new Integer[str1.length][str2.length];
+        
+        System.out.println(LCS(str1.length - 1, str2.length - 1));
+    }
+    
+    static int LCS(int x, int y){
+        if(x == -1 || y == -1){
+            return 0;
+        }
+        
+        if(dp[x][y] == null){
+            dp[x][y] = 0;
+            
+            if(str1[x] == str2[y]){
+                dp[x][y] = LCS(x - 1, y - 1) + 1;
+            }
+            
+            else{
+                dp[x][y] = Math.max(LCS(x - 1, y), LCS(x, y - 1));
+            }
+        }
+        
+        return dp[x][y];
+    }
+}
