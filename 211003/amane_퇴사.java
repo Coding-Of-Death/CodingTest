1b213bb7203bda468993cbe27e83862c8066e094
이주은 <48676089+amane-IT@users.noreply.github.com>
2021-12-15 00:59:57 +0900

    Create amane_퇴사.java

---
+++ 211003/amane_퇴사.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+    	
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int n = sc.nextInt();
+    	
+    	int[] t = new int[n];
+    	int[] p = new int[n];
+    	int[] dp = new int[n + 1];
+    	
+    	for(int i = 0; i < n; i++) {
+    		t[i] = sc.nextInt();
+    		p[i] = sc.nextInt();
+    	}
+    	
+    	for(int i = 0; i < n; i++) {
+    		if(i + t[i] <= n)
+    			dp[t[i] + i] = Math.max(dp[t[i] + i], p[i] + dp[i]);
+    		
+    		dp[i + 1] = Math.max(dp[i], dp[i + 1]);
+    	}
+    	
+    	System.out.println(dp[n]);
+	}
+}
