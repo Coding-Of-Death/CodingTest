e3c80273a073f987d118481031ffcb9d3f8f7d3c
sihwa1019 <62604614+sihwa1019@users.noreply.github.com>
2021-12-17 23:22:50 +0900

    Create sihwa_회전하는큐.java

---
+++ 210815/sihwa_회전하는큐.java
@@ -0,0 +1,52 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        LinkedList<Integer> deque = new LinkedList<Integer>();
+        
+        int count = 0;
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        for(int i = 1 ; i <= n; i++){
+            deque.offer(i);
+        }
+        
+        int[] seq = new int[m];
+        
+        for(int i = 0 ; i<m; i++){
+            seq[i] = sc.nextInt();
+        }
+        
+        for(int i = 0 ; i < m; i++){
+            int target_idx = deque.indexOf(seq[i]);
+            int half_idx;
+        
+        
+        if(deque.size()%2==0){
+            half_idx = deque.size()/2-1;
+        }else{
+            half_idx = deque.size()/2;
+        }
+        
+        
+        if(target_idx <= half_idx){
+            for(int j =0 ; j < target_idx;j++){
+               int temp = deque.pollFirst();
+               deque.offerLast(temp);
+               count++;
+            }
+        }else{
+            for(int j = 0 ; j< deque.size() - target_idx; j++){
+                int temp = deque.pollLast();
+                deque.offerFirst(temp);
+                count++;
+            }
+        }
+        deque.pollFirst();
+       }
+        System.out.println(count);
+    }
+    
+}
