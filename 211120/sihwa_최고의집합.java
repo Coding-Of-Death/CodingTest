433314ad0fd15e5bbbc7519e3a65a63bc735bf85
sihwa1019 <62604614+sihwa1019@users.noreply.github.com>
2021-12-26 23:09:55 +0900

    Create sihwa_최고의집합.java

---
+++ 211120/sihwa_최고의집합.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, int s) {
+        int[] answer = {};
+        if(n>s){
+            return new int[]{-1};
+        }
+            answer = new int[n];
+            for(int i = 0; i < answer.length;i++){
+                answer[i] = s/n;
+            }
+            for(int i = 0 ; i < s%n; i++){
+                answer[i] ++;
+            }
+            Arrays.sort(answer);
+        
+        return answer;
+    }
+}
