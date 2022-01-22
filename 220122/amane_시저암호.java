91977f6fb601875f4e65dec17a483b51c4d162b0
이주은 <48676089+amane-IT@users.noreply.github.com>
2022-01-22 17:25:08 +0900

    Create amane_시저암호.java

---
+++ 220122/amane_시저암호.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String s, int n) {
+        String answer = "";
+        
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            
+            if(Character.isLowerCase(ch))
+                answer += (char)((ch - 'a' + n) % 26 + 'a');
+            
+            else if (Character.isUpperCase(ch))
+                answer += (char) ((ch - 'A' + n) % 26 + 'A');
+            else
+                answer += ch;
+        }
+        return answer;
+    }
+}
